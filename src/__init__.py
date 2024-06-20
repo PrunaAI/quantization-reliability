b0da30bfd073e3704fc293c323d2b6b3a5683565
Sándor Daróczi <daroczisandor00@gmail.com>
2024-06-20 21:39:42 +0200

    fixed save path

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-SAVE_PATH_BASE = "nfs/students/daro"
+SAVE_PATH_BASE = "/nfs/students/daro"
 MODEL_SAVE_PATH = os.path.join(SAVE_PATH_BASE, "models")
 RESULTS_SAVE_PATH = os.path.join(SAVE_PATH_BASE, "results")
 DATA_SAVE_PATH = os.path.join(SAVE_PATH_BASE, "data")
