8e0bd08625b207e5d64b444019b816066a3a9d9b
Sándor Daróczi <daroczisandor00@gmail.com>
2024-07-04 13:40:51 +0200

    fix quotes hf auth

--- src/run_quantize.py
+++ src/run_quantize.py
@@ -58,7 +58,7 @@ huggingface_token = os.getenv('HUGGINGFACE_TOKEN')
 if huggingface_token is None:
     raise ValueError(
         f"Please set the HUGGINGFACE_TOKEN environment variable."
-        f"Looking in {os.path.join(os.getcwd(), ".env")}"
+        f"Looking in {os.path.join(os.getcwd(), '.env')}"
     )
 else:
     logging.info("Hugging Face token loaded successfully.")
