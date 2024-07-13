8b79275e3461474ed2b2e5a592b068e015e5b0e4
Sándor Daróczi <daroczisandor00@gmail.com>
2024-07-13 19:01:59 +0200

    implement hqq+

--- src/algorithms/quantization/__init__.py
+++ src/algorithms/quantization/__init__.py
@@ -1,10 +1,15 @@
+import torch
 from hqq.core.quantize import BaseQuantizeConfig as HQQBaseQuantizeConfig
+
+##### NONE
 
 NONE = {
     "name": "NONE",
     "quantize_method": "NONE",
     "num_bits": 16
 }
+
+##### BNB 4-bit
 
 BNB_4 = {
     "name": "BNB-4",
@@ -16,6 +21,8 @@ BNB_4 = {
     "bnb_4bit_use_double_quant": False
 }
 
+##### BNB 8-bit
+
 BNB_8 = {
     "name": "BNB-8",
     "quantize_method": "BNB",
@@ -25,6 +32,8 @@ BNB_8 = {
     "llm_int8_enable_fp32_cpu_offload": False,
     "llm_int8_has_fp16_weight": False
 }
+
+##### AWQ 4-bit
 
 AWQ_4 = {
     "name": "AWQ-4",
@@ -36,6 +45,8 @@ AWQ_4 = {
     "version": "GEMM"
 }
 
+##### HQQ 8-bit uniform
+
 HQQ_8_uniform = {
     "name": "HQQ-8-uniform",
     "quantize_method": "HQQ",
@@ -45,6 +56,8 @@ HQQ_8_uniform = {
     "quant_scale": False,
     "axis": 0
 }
+
+##### HQQ mixed
 
 # Define 3-bit and 4-bit configurations separately
 HQQ_q4_config = HQQBaseQuantizeConfig(
@@ -74,11 +87,70 @@ HQQ_mixed = {
     "mlp.down_proj": HQQ_q3_config
 }
 
+##### HQQ+
+
+hqq_plus_base_params = {
+    'nbits': 4,
+    'group_size': 64,
+    'quant_scale': False,
+    'quant_zero': False
+}
+
+train_dtype = torch.float32
+lora_base_params = {
+    'lora_type': 'default',
+    'r': 32,
+    'lora_alpha': 64,
+    'dropout': 0.05,
+    'train_dtype': train_dtype
+}
+
+lora_params = {
+    'self_attn.q_proj': lora_base_params,
+    'self_attn.k_proj': lora_base_params,
+    'self_attn.v_proj': lora_base_params,
+    'self_attn.o_proj': lora_base_params,
+    'mlp.gate_proj': None,
+    'mlp.up_proj': None,
+    'mlp.down_proj': None
+}
+
+# Fine-tuning training hyperparameters
+fine_tuning_params = {
+    'grad_acc': 2,
+    'logging_st': 1,
+    'max_steps': -1,
+    'lr': 1e-4,
+    'batch_size': 1,
+    'n_epochs': 1,
+    'output_dir': '.',
+    'fp16': (train_dtype == torch.float32),
+    'max_grad_norm': 1.0,
+    'save_steps': 10000000,
+    'lr_scheduler_type': "linear",
+    'max_tokens': 256,
+    'max_samples': 5000,
+    'random_seed': 100,
+    'eval_dataset': None,
+    'peft_config': None,
+    'packing': True
+}
+
+HQQ_LORA = {
+    "name": "HQQ-LORA",
+    "quantize_method": "HQQ",
+    "num_bits": 4,
+    "base_params": hqq_plus_base_params,
+    "lora_params": lora_params,
+    "fine_tuning_params": fine_tuning_params
+}
+
 QUANT_CONFIGS = {
-  "NONE": NONE,
-  "BNB-4": BNB_4,
-  "BNB-8": BNB_8,
-  "AWQ-4": AWQ_4,
-  "HQQ-8-uniform": HQQ_8_uniform,
-  "HQQ-mixed": HQQ_mixed
+    "NONE": NONE,
+    "BNB-4": BNB_4,
+    "BNB-8": BNB_8,
+    "AWQ-4": AWQ_4,
+    "HQQ-8-uniform": HQQ_8_uniform,
+    "HQQ-mixed": HQQ_mixed,
+    "HQQ-LORA": HQQ_LORA
 }
--- src/algorithms/quantization/hqq_plus.py
+++ src/algorithms/quantization/hqq_plus.py
@@ -1,63 +1,134 @@
 import os
 import time
 import torch
-from transformers import AutoModelForCausalLM
-from hqq.models.hf.base import AutoHQQHFModel
+from transformers import TrainingArguments
+from hqq.engine.hf import HQQModelForCausalLM, AutoTokenizer
+from hqq.models.hf.llama import LlamaHQQ
 from hqq.core.quantize import BaseQuantizeConfig as HQQBaseQuantizeConfig
+from hqq.core.peft import PeftUtils
+from datasets import Dataset
+from trl import SFTTrainer
+from tqdm import tqdm
+import numpy as np
+import random
 from src import MODEL_SAVE_PATH
 
-def quantize_hqq_plus(model_name, quantize_config={}, save_model=False, save_path="", device="cuda"):
+def quantize_hqq_plus(model_name, tokenizer, calib_dataloader, quantize_config={}, save_model=False, save_path="", device="cuda"):
     if quantize_config['num_bits'] is None or quantize_config['num_bits'] not in [4, 8]:
         raise ValueError(f"Invalid num_bits for HQQ: {quantize_config['num_bits']}")
 
+    calib_dataset = calib_dataloader.ORIGINAL_DATASET
     hqq_config = {}
-    # Option 1: All linear layers will use the same quantization config
-    if quantize_config['name'] == "HQQ-mixed":
-        hqq_config = {
-            'self_attn.q_proj': quantize_config['self_attn.q_proj'],
-            'self_attn.k_proj': quantize_config['self_attn.k_proj'],
-            'self_attn.v_proj': quantize_config['self_attn.v_proj'],
-            'self_attn.o_proj': quantize_config['self_attn.o_proj'],
-            'mlp.gate_proj': quantize_config['mlp.gate_proj'],
-            'mlp.up_proj': quantize_config['mlp.up_proj'],
-            'mlp.down_proj': quantize_config['mlp.down_proj'],
-        }
-    elif quantize_config['name'] == "HQQ-8-uniform":
-        hqq_config = HQQBaseQuantizeConfig(
-            nbits=quantize_config['num_bits'],
-            group_size=quantize_config['group_size'],
-            quant_zero=quantize_config["quant_zero"],
-            quant_scale=quantize_config["quant_scale"],
-            axis=quantize_config["axis"]
-        )
+    if quantize_config['name'] == "HQQ-LORA":
+        hqq_config = quantize_config
     else:
         raise ValueError(f"Invalid quantize_config for HQQ: {quantize_config['name']}")
     
-    start_time = time.time()
-    hqq_model_base = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="cuda")
-    hqq_model = AutoHQQHFModel.quantize_model(
-        hqq_model_base,
-        quant_config=hqq_config,
-        compute_dtype=torch.float16,
-        device=device
-    )
-    end_time = time.time()  # End time measurement
-    hqq_model.QUANT_TIME = end_time - start_time
+    hqq_plus_base_params = hqq_config['hqq_plus_base_params']
+    lora_params = hqq_config['lora_params']
+    fine_tuning_params = hqq_config['fine_tuning_params']    
+    
+    # Load model and tokenizer
+    print(f"Loading model {model_name}")
+    model = HQQModelForCausalLM.from_pretrained(model_name, cache_dir=None, torch_dtype="auto", device_map=device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, truncation=True, padding=True, cache_dir=None)
 
+    # Quantize the model
+    start_time = time.time()
+    quant_config = HQQBaseQuantizeConfig(**hqq_plus_base_params)
+    model.quantize_model(quant_config=quant_config)
+
+    # Save model name and path
     hqq_model_name = f"{model_name.split('/')[1]}-{quantize_config['name']}"
     hqq_model_path = os.path.join(MODEL_SAVE_PATH, hqq_model_name)
-    hqq_model.PATH = hqq_model_path
-    hqq_model.NAME = hqq_model_name
+    model.NAME = hqq_model_name
+    model.PATH = hqq_model_path
+
+    # Add LoRA
+    print("Adding LoRA to model")
+    PeftUtils.add_lora(model, lora_params)
+
+    # Prepare dataset
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    tokenizer.add_bos_token = False
+    tokenizer.add_eos_token = False
     
-    print(f'Model {model_name} is quantized to {hqq_model_name}')
-    
-    # Calculate model size and GPU utilization
-    # calculate_model_size(bnb_model_path)
-    # from src.models.utils_llm import print_gpu_utilization
-    # print_gpu_utilization()
+    def pre_process_chat(chat):
+        return chat
+
+    random.seed(100)
+    idx = random.sample(range(len(calib_dataset)), min(fine_tuning_params['max_samples'], len(calib_dataset)))
+    calib_dataset = Dataset.from_dict({'text': [pre_process_chat(calib_dataset[i]['text']) for i in tqdm(idx)]})
+    # dataset_val = Dataset.from_dict({'text': [pre_process_chat(dataset_val[i]['text']) for i in range(len(dataset_val))]})
+
+    # Train
+    training_args = TrainingArguments(
+        output_dir=fine_tuning_params['output_dir'],
+        per_device_train_batch_size=fine_tuning_params['batch_size'],
+        gradient_accumulation_steps=fine_tuning_params['grad_acc'],
+        learning_rate=fine_tuning_params['lr'],
+        logging_steps=fine_tuning_params['logging_st'],
+        num_train_epochs=fine_tuning_params['n_epochs'],
+        max_steps=fine_tuning_params['max_steps'],
+        remove_unused_columns=False,
+        fp16=fine_tuning_params['fp16'],
+        max_grad_norm=fine_tuning_params['max_grad_norm'],
+        save_steps=fine_tuning_params['save_steps'],
+        lr_scheduler_type=fine_tuning_params['lr_scheduler_type'],
+    )
+
+    class WrappedModel(torch.nn.Module):
+        def __init__(self, model):
+            super().__init__()
+            self.model = model
+
+        def forward(self, *args, **kwargs):
+            return self.model.forward(*args, **kwargs)
+
+        def train(self):
+            self.model.train()
+
+        def eval(self):
+            self.model.eval()
+
+        def parameters(self):
+            return self.model.parameters()
+
+    trainer = SFTTrainer(
+        model=WrappedModel(model),
+        tokenizer=tokenizer,
+        max_seq_length=fine_tuning_params['max_tokens'],
+        train_dataset=calib_dataset,
+        eval_dataset=fine_tuning_params['eval_dataset'],
+        peft_config=fine_tuning_params['peft_config'],
+        packing=fine_tuning_params['packing'],
+        args=training_args,
+        dataset_text_field="text",
+    )
+
+    model.is_parallelizable = False
+    trainer.is_model_parallel = False
+    trainer.place_model_on_device = False
+
+    print("Training model")
+    model.train()
+    try:
+        trainer.train()
+    except TypeError as e:
+        print(f"Run into error while saving model: {e}")
+        
+    end_time = time.time()
+    model.QUANT_TIME = end_time - start_time
+
+    print(f"Setting model to eval mode")
+    model.eval()
+    print("Casting LoRA weights to model dtype")
+    PeftUtils.cast_lora_weights(model, dtype=torch.float32)
     
     if save_model:
         save_dir = save_path if save_path else hqq_model_path
-        AutoHQQHFModel.save_quantized(hqq_model, save_dir)
-
-    return hqq_model
+        PeftUtils.save_lora_weights(model, save_dir)
+        PeftUtils.load_lora_weights(model, save_dir, base_class=LlamaHQQ)
+    
+    return model
