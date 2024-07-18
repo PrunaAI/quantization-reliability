79acf70714d90c74c99d9d55864c678d5e7b29dc
Sándor Daróczi <daroczisandor00@gmail.com>
2024-07-18 16:40:37 +0200

    shuffle=True

--- src/data/C4DataModule.py
+++ src/data/C4DataModule.py
@@ -1,110 +1,117 @@
-import copy
 import os
-import random
 from pytorch_lightning import LightningDataModule
+from torch import device
+from torch._C import device
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoTokenizer
 from datasets import load_dataset
 
 
+# TODO: This dataset merge all independent sentences and process them as a single batch sample.
+#  This is not ideal since sometimes sentences might switch from a topic to another.
+#  However, it was done similarly on Wanda and SparseGPT code.
 class TextDataset(Dataset):
-    def __init__(self, dataset, tokenizer, n_samples=128, text_key="text", sequence_length=2048, seed=0):
+    def __init__(self, dataset, tokenizer, text_key="text", sequence_length=2048, stride=512, seed=0):
         self.tokenizer = tokenizer
         self.dataset = dataset[:2000]
         self.texts = dataset[text_key][:2000]
-        if n_samples is None:
-            n_samples = len(self.dataset)
-        self.n_samples = n_samples
         self.sequence_length = sequence_length
+        self.stride = stride
         self.seed = seed
 
         # Tokenize the entire dataset text
         tokenized_dataset = self.tokenizer("\n\n".join(self.texts), return_tensors="pt")
-        self.data = tokenized_dataset.input_ids[0]
+        self.data = tokenized_dataset.input_ids[0, :-1]
         self.labels = tokenized_dataset.input_ids[0]
 
-        # Random sampling for indices
-        random.seed(self.seed)
-        self.indices = []
-        for _ in range(n_samples):
-            start_idx = random.randint(0, len(self.data) - sequence_length - 1)
-            self.indices.append(start_idx)
-
     def __len__(self):
-        return len(self.indices)
+        return len(self.data) // self.stride
 
-    def __getitem__(self, index):
-        start_index = self.indices[index]
-        end_index = start_index + self.sequence_length
-
+    def __getitem__(self, index):        
+        start_index = max(index * self.stride + self.stride - self.sequence_length, 0)
+        end_index = start_index + self.stride
+        if end_index > len(self.data):
+            raise IndexError("Index out of bounds")
         input_ids = self.data[start_index:end_index]
-        target_ids = copy.deepcopy(self.labels[start_index + 1 : end_index + 1])
+        target_ids = self.labels[start_index + 1: end_index + 1]
         target_ids[:-1] = -100
 
         return input_ids, target_ids
 
 
 class C4DataModule(LightningDataModule):
-    def __init__(self, directory_dataset=os.getcwd(), batch_size=1, sequence_length=2048, n_samples=128, tokenizer_name=None, seed=1):
+    def __init__(self, directory_dataset=os.getcwd(), batch_size=64, sequence_length=2048, stride=512, n_lines=None, tokenizer_name=None, seed=1):
         super().__init__()
         self.directory_dataset = directory_dataset
         self.batch_size = batch_size
-        self.n_samples = n_samples
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, legacy=False)
-        self.sequence_length = sequence_length
+        self.sequence_length = sequence_length#
+        self.stride = stride
+        self.n_lines = n_lines
         self.prepare_data()
 
     def prepare_data(self):
+        # Load train, val, and test datasets
+        train_split = "train" if self.n_lines is None else f"train[:{self.n_lines}]"
+        validation_split = "validation" if self.n_lines is None else f"validation[:{self.n_lines}]"
+        test_split = "validation" if self.n_lines is None else f"validation[{self.n_lines}:{2*self.n_lines}]"
+        
         self.train_dataset = load_dataset(
             "allenai/c4",
             data_files={"train": "en/c4-train.00000-of-01024.json.gz"},
-            split="train"
+            split=train_split
         )
         self.val_dataset = load_dataset(
             "allenai/c4",
             data_files={"validation": "en/c4-validation.00000-of-00008.json.gz"},
-            split="validation",
+            split=validation_split,
         )
         # C4 does not have an explicit test set for now. We use the validation set instead.
         # Check https://github.com/locuslab/wanda/blob/8e8fc87b4a2f9955baa7e76e64d5fce7fa8724a6/lib/data.py#L63 for other examples.
         self.test_dataset = load_dataset(
             "allenai/c4",
             data_files={"validation": "en/c4-validation.00001-of-00008.json.gz"},
-            split="validation",
+            split=test_split,
         )
 
-    def train_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def train_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.train_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.train_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         train_dataloader.ORIGINAL_DATASET = self.train_dataset
         return train_dataloader
 
-    def val_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def val_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.val_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.val_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         val_dataloader.ORIGINAL_DATASET = self.val_dataset
         return val_dataloader
 
-    def test_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def test_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.test_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.test_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         test_dataloader.ORIGINAL_DATASET = self.test_dataset
         return test_dataloader
--- src/data/WikiTextDataModule.py
+++ src/data/WikiTextDataModule.py
@@ -8,14 +8,12 @@ from datasets import load_dataset
 
 
 class TextDataset(Dataset):
-    def __init__(self, dataset, tokenizer, n_samples=128, text_key="text", sequence_length=2048, seed=0):
+    def __init__(self, dataset, tokenizer, text_key="text", sequence_length=2048, stride=512, seed=0):
         self.tokenizer = tokenizer
         self.dataset = dataset
         self.texts = dataset[text_key]
-        if n_samples is None:
-            n_samples = len(self.dataset)
-        self.n_samples = n_samples
         self.sequence_length = sequence_length
+        self.stride = stride
         self.seed = seed
 
         # Tokenize the entire dataset text
@@ -23,74 +21,79 @@ class TextDataset(Dataset):
         self.data = tokenized_dataset.input_ids[0, :-1]
         self.labels = tokenized_dataset.input_ids[0]
 
-        # Random sampling for indices
-        random.seed(self.seed)
-        self.indices = []
-        for _ in range(n_samples):
-            start_idx = random.randint(0, len(self.dataset) - sequence_length - 1)
-            self.indices.append(start_idx)
-
     def __len__(self):
-        return len(self.indices)
+        return len(self.data) // self.stride
 
-    def __getitem__(self, index):
-        start_index = self.indices[index]
-        end_index = start_index + self.sequence_length
-
+    def __getitem__(self, index):        
+        start_index = max(index * self.stride + self.stride - self.sequence_length, 0)
+        end_index = start_index + self.stride
+        if end_index > len(self.data):
+            raise IndexError("Index out of bounds")
         input_ids = self.data[start_index:end_index]
-        target_ids = copy.deepcopy(self.labels[start_index + 1 : end_index + 1])
+        target_ids = self.labels[start_index + 1: end_index + 1]
         target_ids[:-1] = -100
 
         return input_ids, target_ids
 
 
 class WikiTextDataModule(LightningDataModule):
-    def __init__(self, directory_dataset=os.getcwd(), batch_size=1, sequence_length=2048, n_samples=128, tokenizer_name=None, seed=1):
+    def __init__(self, directory_dataset=os.getcwd(), batch_size=1, sequence_length=2048, stride=512, n_lines=None, tokenizer_name=None, seed=1):
         super().__init__()
         self.directory_dataset = directory_dataset
         self.batch_size = batch_size
-        self.n_samples = n_samples
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, legacy=False)
+        self.n_lines = n_lines
+        self.stride = stride
         self.sequence_length = sequence_length
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, legacy=False)
         self.prepare_data()
 
     def prepare_data(self):
+        train_split = "train" if self.n_lines is None else f"train[:{self.n_lines}]"
+        validation_split = "validation" if self.n_lines is None else f"validation[:{self.n_lines}]"
+        test_split = "test" if self.n_lines is None else f"test[:{self.n_lines}]"
+        
         self.train_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
         self.val_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="validation")
         self.test_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
 
-    def train_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def train_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.train_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.train_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         train_dataloader.ORIGINAL_DATASET = self.train_dataset
         return train_dataloader
 
-    def val_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def val_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.val_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.val_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        val_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         val_dataloader.ORIGINAL_DATASET = self.val_dataset
         return val_dataloader
 
-    def test_dataloader(self, batch_size=None, sequence_length=None, n_samples=None):
+    def test_dataloader(self, batch_size=None, sequence_length=None, stride=None):
         if batch_size is None:
             batch_size = self.batch_size
-        if n_samples is None:
-            n_samples = self.n_samples
         if sequence_length is None:
             sequence_length = self.sequence_length
-        dataset = TextDataset(self.test_dataset, tokenizer=self.tokenizer, n_samples=n_samples, sequence_length=sequence_length)
-        test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        else:
+            sequence_length = min(self.sequence_length, sequence_length)
+        if stride is None:
+            stride = self.stride
+        dataset = TextDataset(self.test_dataset, tokenizer=self.tokenizer, sequence_length=sequence_length, stride=stride)
+        test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         test_dataloader.ORIGINAL_DATASET = self.test_dataset
         return test_dataloader
