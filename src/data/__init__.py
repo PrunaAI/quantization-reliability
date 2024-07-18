b93277cc7356edf2098e25950b3479bd3ff03333
Sándor Daróczi <daroczisandor00@gmail.com>
2024-07-18 16:55:51 +0200

    remove n_samples, add stride

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -13,77 +13,77 @@ logger = logging.getLogger("quant_logger")
 
 # Define the base datasets dictionary
 base_datasets = {
-    "Polyglot": lambda directory_dataset, batch_size, n_samples, sequence_length, tokenizer_name, seed, **kwargs: PolyglotDataModule(
+    "Polyglot": lambda directory_dataset, batch_size, sequence_length, stride, tokenizer_name, seed, **kwargs: PolyglotDataModule(
         directory_dataset=directory_dataset,
         batch_size=batch_size,
-        n_samples=n_samples,
         sequence_length=sequence_length,
+        stride=stride,
         tokenizer_name=tokenizer_name,
         seed=seed,
     ),
-    "WikiText": lambda directory_dataset, batch_size, n_samples, sequence_length, tokenizer_name, seed, **kwargs: WikiTextDataModule(
+    "WikiText": lambda directory_dataset, batch_size, sequence_length, stride, tokenizer_name, seed, **kwargs: WikiTextDataModule(
         directory_dataset=directory_dataset,
         batch_size=batch_size,
-        n_samples=n_samples,
         sequence_length=sequence_length,
-        tokenizer_name=tokenizer_name,
-        seed=seed,
-        **kwargs,
-    ),
-    "OpenAssistant": lambda directory_dataset, batch_size, n_samples, sequence_length, tokenizer_name, seed, **kwargs: OpenAssistantDataModule(
-        directory_dataset=directory_dataset,
-        batch_size=batch_size,
-        n_samples=n_samples,
-        sequence_length=sequence_length,
+        stride=stride,
         tokenizer_name=tokenizer_name,
         seed=seed,
         **kwargs,
     ),
-    "C4": lambda directory_dataset, batch_size, n_samples, sequence_length, tokenizer_name, seed, **kwargs: C4DataModule(
+    "OpenAssistant": lambda directory_dataset, batch_size, sequence_length, stride, tokenizer_name, seed, **kwargs: OpenAssistantDataModule(
         directory_dataset=directory_dataset,
         batch_size=batch_size,
-        n_samples=n_samples,
         sequence_length=sequence_length,
+        stride=stride,
         tokenizer_name=tokenizer_name,
         seed=seed,
         **kwargs,
     ),
-    "PTB": lambda directory_dataset, batch_size, n_samples, sequence_length, tokenizer_name, seed, **kwargs: PTBDataModule(
+    "C4": lambda directory_dataset, batch_size, sequence_length, stride, tokenizer_name, seed, **kwargs: C4DataModule(
         directory_dataset=directory_dataset,
         batch_size=batch_size,
-        n_samples=n_samples,
         sequence_length=sequence_length,
+        stride=stride,
+        tokenizer_name=tokenizer_name,
+        seed=seed,
+        **kwargs,
+    ),
+    "PTB": lambda directory_dataset, batch_size, sequence_length, stride, tokenizer_name, seed, **kwargs: PTBDataModule(
+        directory_dataset=directory_dataset,
+        batch_size=batch_size,
+        sequence_length=sequence_length,
+        stride=stride,
         tokenizer_name=tokenizer_name,
         seed=seed,
         **kwargs,
     ),
 }
 
-def data_loader_from_split(data_module, split, batch_size=None, n_samples=None, sequence_length=None):
+def data_loader_from_split(data_module, split, batch_size=None, sequence_length=None, stride=None):
     if split == "train":
-        return data_module.train_dataloader(batch_size=batch_size, n_samples=n_samples, sequence_length=sequence_length)
+        return data_module.train_dataloader(batch_size=batch_size, sequence_length=sequence_length, stride=stride)
     elif split == "validation":
-        return data_module.val_dataloader(batch_size=batch_size, n_samples=n_samples, sequence_length=sequence_length)
+        return data_module.val_dataloader(batch_size=batch_size, sequence_length=sequence_length, stride=stride)
     elif split == "test":
-        return data_module.test_dataloader(batch_size=batch_size, n_samples=n_samples, sequence_length=sequence_length)
+        return data_module.test_dataloader(batch_size=batch_size, sequence_length=sequence_length, stride=stride)
     else:
         raise MisconfigurationException(f"Split {split} is not valid. Must be one of ['train', 'validation', 'test']")
 
-def get_dataset(dataset_name, directory_dataset, batch_size=1, n_samples=128, sequence_length=512, seed=123, tokenizer_name=None, **kwargs):
+def get_dataset(dataset_name, directory_dataset, batch_size=1, sequence_length=2048, stride=512, seed=123, tokenizer_name=None, **kwargs):
     # Get dataset
     if dataset_name in base_datasets:
         logger.info(f"Loading dataset {dataset_name} with the following configuration:")
         logger.info(f"  directory_dataset: {directory_dataset}")
         logger.info(f"  batch_size: {batch_size}")
-        logger.info(f"  n_samples: {n_samples}")
         logger.info(f"  sequence_length: {sequence_length}")
+        logger.info(f"  stride: {stride}")
         logger.info(f"  seed: {seed}")
         logger.info(f"  tokenizer_name: {tokenizer_name}")
         return base_datasets[dataset_name](
             directory_dataset=directory_dataset,
             batch_size=batch_size,
-            n_samples=n_samples,
             sequence_length=sequence_length,
+            stride=stride,
             tokenizer_name=tokenizer_name,
             seed=seed,
             **kwargs
