082b747cfce2242854e32450d8c296c58415e237
Sándor Daróczi <daroczisandor00@gmail.com>
2024-07-25 17:06:51 +0200

    implement aqlm scripts

---
+++ src/algorithms/quantization/aqlm_lora.py
@@ -0,0 +1,89 @@
+import os
+import time
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import LoraConfig, get_peft_model
+import transformers
+from src import MODEL_SAVE_PATH
+
+def quantize_aqlm_lora(model_name, tokenizer, calib_dataloader, quantize_config={}, save_model=False, save_path="", device="cuda"):
+    if quantize_config['num_bits'] is None or quantize_config['num_bits'] not in [4, 8]:
+        raise ValueError(f"Invalid num_bits for BNB: {quantize_config['num_bits']}")
+    
+    lora_params = quantize_config['lora_params']
+    fine_tuning_params = quantize_config['fine_tuning_params']
+    # Validate the LoRA configuration
+    required_keys = ['r', 'lora_alpha', 'target_modules', 'lora_dropout', 'bias', 'task_type']
+    for key in required_keys:
+        if key not in lora_params:
+            raise ValueError(f"Missing required LoRA config key: {key}")
+
+    # Load the tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(model_name, device_map=device)
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map=device)
+    model.NAME = model_name
+
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
+
+    # Configure and apply LoRA
+    lora_params = LoraConfig(
+        r=lora_params['r'],
+        lora_alpha=lora_params['lora_alpha'],
+        target_modules=lora_params['target_modules'],
+        lora_dropout=lora_params['lora_dropout'],
+        bias=lora_params['bias'],
+        task_type=lora_params['task_type']
+    )
+    
+    model = get_peft_model(model, lora_params)
+    model.print_trainable_parameters()
+    model.enable_input_require_grads()  # Needed for gradient checkpointing
+
+    # Load and preprocess dataset
+    calib_dataset = calib_dataloader.ORIGINAL_DATASET
+    calib_dataset = calib_dataset.map(lambda samples: tokenizer(samples["text"]), batched=True)
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Define Trainer using fine-tuning parameters
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=calib_dataset,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=fine_tuning_params['batch_size'],
+            gradient_accumulation_steps=fine_tuning_params['grad_acc'],
+            gradient_checkpointing=True,
+            warmup_steps=2,
+            max_steps=fine_tuning_params['max_steps'],
+            learning_rate=fine_tuning_params['lr'],
+            fp16=fine_tuning_params['fp16'],
+            logging_steps=fine_tuning_params['logging_st'],
+            output_dir=fine_tuning_params['output_dir'],
+            optim="adamw_bnb_8bit",
+            logging_first_step=True,
+        ),
+        data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+    )
+    model.config.use_cache = False  # Silence the warnings. Please re-enable for inference!
+    
+    # Start finetuning
+    start_time = time.time()
+    trainer.train()
+    end_time = time.time()  # End time measurement
+    model.QUANT_TIME = end_time - start_time
+
+    model_name_suffix = f"{model_name.split('/')[1]}-LoRA"
+    model_path = os.path.join(MODEL_SAVE_PATH, model_name_suffix)
+    model.PATH = model_path
+    model.NAME = model_name_suffix
+    
+    print(f'Model {model_name} is quantized and finetuned to {model_name_suffix}')
+
+    # Save model if specified
+    if save_model:
+        save_dir = save_path if save_path else model_path
+        model.save_pretrained(save_dir)
+        tokenizer.save_pretrained(save_dir)
+    
+    return model
---
+++ src/algorithms/quantization/aqlm_prequantized.py
@@ -0,0 +1,25 @@
+import time
+import torch
+from transformers import AutoModelForCausalLM
+
+def quantize_aqlm_prequantized(quantize_config={}, device="cuda"):
+    if 'model_variant' not in quantize_config:
+        raise ValueError("Missing required key 'model_variant' in quantize_config")
+    
+    # Validate the model_variant
+    if quantize_config['model_variant'] not in quantize_config['model_variants']:
+        raise ValueError(f"Invalid model_variant: {quantize_config['model_variant']}")
+
+    # Load the tokenizer and model
+    start_time = time.time()
+    model = AutoModelForCausalLM.from_pretrained(quantize_config['model_variant'], torch_dtype="auto", device_map=device)
+    end_time = time.time()  # End time measurement
+    model.QUANT_TIME = end_time - start_time
+    
+    # Add additional attributes to the model
+    model.NAME = quantize_config['model_variant']
+    
+    print(f'Model {quantize_config['model_variant']} is loaded.')
+    
+    return model
+
